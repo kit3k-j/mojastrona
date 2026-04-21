d26be121e0d254e356688682a292338425fa5200
kit3k-j <jakubkramarczyk2012@gmail.com>
2026-04-21 20:48:53 +0200

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask, request
-import os
 from datetime import datetime
+import requests
+import os
 
 app = Flask(__name__)
 
+LOG_FILE = "ip_log.txt"
+
+def get_location(ip):
+    try:
+        res = requests.get(f"http://ip-api.com/json/{ip}").json()
+        return f"{res.get('country')} - {res.get('city')}"
+    except:
+        return "unknown"
+
 def save_ip(ip):
-    with open("ip_log.txt", "a") as f:
-        f.write(f"{datetime.now()} - {ip}\n")
+    location = get_location(ip)
+    with open(LOG_FILE, "a") as f:
+        f.write(f"{datetime.now()} - {ip} - {location}\n")
+
+def read_logs():
+    try:
+        with open(LOG_FILE, "r") as f:
+            return f.readlines()
+    except:
+        return []
 
 @app.route("/")
 def home():
@@ -14,8 +32,19 @@ def home():
     save_ip(ip)
 
     return f"""
-    <h1>Właśnie zmarnowales jakies 5 sekund życia 😄</h1>
-    <p>Twoje IP(jakbyś zaponiał): {ip}</p>
+    <h1>Strona działa 😄</h1>
+    <p>Twoje IP: {ip}</p>
+    <a href="/panel">Panel</a>
+    """
+
+@app.route("/panel")
+def panel():
+    logs = read_logs()
+    logs_html = "<br>".join(logs)
+
+    return f"""
+    <h1>Panel IP 🌍</h1>
+    <pre>{logs_html}</pre>
     """
 
 if __name__ == "__main__":
